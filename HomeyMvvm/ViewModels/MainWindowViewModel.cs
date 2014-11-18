e87b28db8515b3368711731a6aa450fe0f96d6f1
amitthk <kth.amit@gmail.com>
2014-11-18 19:29:52 +0800

    Added NotifyIconHelper and NotifyIcon service

--- HomeyMvvm/ViewModels/MainWindowViewModel.cs
+++ HomeyMvvm/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace HomeyMvvm.ViewModels
@@ -15,14 +16,19 @@ namespace HomeyMvvm.ViewModels
     public class MainWindowViewModel : BaseViewModel
     {
         private readonly ICommand _ExitCmd;
+        private readonly ICommand _MinimizeToTrayCmd;
+
 
         public ICommand ExitCmd { get { return (_ExitCmd); } }
+        public ICommand MinimizeToTrayCmd { get { return (_MinimizeToTrayCmd); } }
+
 
 
         public MainWindowViewModel()
         {
             //Initialize the command
             _ExitCmd = new RelayCommand(ExecExit, CanExit);
+            _MinimizeToTrayCmd = new RelayCommand(ExecMinimizeToTrayCmd, CanMinimizeToTray);
         }
 
 
@@ -37,6 +43,23 @@ namespace HomeyMvvm.ViewModels
         {
             //Todo: Add the checking for CanExit Here
             return (true);
+        }
+
+        //These methods are checked again & again in loop, so we will step through them while debugging
+        [DebuggerStepThrough]
+        private bool CanMinimizeToTray(object obj)
+        {
+            return (true);
+        }
+
+        private void ExecMinimizeToTrayCmd(object obj)
+        {
+            var win = (Window)obj;
+
+            var notifyIcon = NotifyIconHelper.Instance.GetIcon(win);
+
+            notifyIcon.Visible = true;
+            win.Hide();
         }
     }
 }
